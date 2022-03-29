37fa66262201ee3b04c23297a8fd0ae9907380c4
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-29 14:56:22 +0200

    mermaid js

--- script.js
+++ script.js
@@ -12,3 +12,18 @@
 // D --> B
 // C -->|si| E[Msg di benvenuto]
 // E --> Z[FINE]
+
+///////////////////////////////////////////
+
+// %% Gioco dei dadi
+// %% Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// %% Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// flowchart TD
+// A[INIZIO] -->|random| B[Genera num random da 1 a 6]
+// B -->|human| C[Il tuo lancio è: ]
+// B -->|machina| D[Il lancio della macchina è: ]
+// C & D --> F{C > D?}
+// F -->|si| G[Il vincitore è: Human]
+// F -->|no| H[Il vincitore è: Machina]
+// G & H --> Z[FINE]
